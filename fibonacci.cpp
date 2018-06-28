cd86f55f43cb74fe03fabb17e91a0efec5c20c0a
unknown <chitturiprasanth13797@gmail.com>
2018-06-28 19:48:47 +0530

    fibonacci file added

---
+++ fibonacci.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a = 0, b = 1, f = 0;
+
+    std::cout << "Enter no of terms: " << '\n';
+    std::cin >> n;
+
+    std::cout << "Fibonacii series is: " << '\n';
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            std::cout << a << " ";
+            continue;
+        }
+        if (i == 1)
+        {
+            std::cout << b << " ";
+            continue;
+        }
+        f = a + b;
+        a = b;
+        b = f;
+
+        std::cout << f << " ";
+    }
+    
+    system("pause");
+    return 0;
+}
