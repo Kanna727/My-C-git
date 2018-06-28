5c7db9b53031871a9cd6d7c3f8e0341e939cd2bb
unknown <chitturiprasanth13797@gmail.com>
2018-06-29 00:50:18 +0530

    edited

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int n, a = 0, b = 1, f = 0;
 
-    std::cout << "Enter number of terms: " << '\n';
+    std::cout << "Enter number of terms to be in fibonacci series: " << '\n';
     std::cin >> n;
 
     std::cout << "Fibonacii series is: " << '\n';
