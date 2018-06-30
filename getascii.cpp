78efdfc5293fb69547d67eab5e97314f2373c542
chitturiprasanth13797@gmail.com <chitturiprasanth13797@gmail.com>
2018-06-30 17:08:12 +0530

    getascii

--- getascii.cpp
+++ getascii.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main()
 {
     char c;
-    //std::cout << "ASCII value of " << c << "is " << int(c);
+    std::cout << "Enter the character: ";
+    std::cin >> c;
+    std::cout << "ASCII value of " << c << " is " << int(c) << endl;
     system("pause");
     return 0;
 }
